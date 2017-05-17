100f3641db668e984fa959c7728415d266ca0daa
Hung Nguyen <hung.nguyen@sentifi.com>
2017-05-17 14:14:47 +0700

    format file

--- webpack.config.js
+++ webpack.config.js
@@ -4,66 +4,51 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        app: './src/app.js',
-        contact: './src/contact.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'
-    },
-    module: {
-        rules: [
-            { 
-                test: /\.scss$/, 
-                use: ['style-loader','css-loader','sass-loader']
-            },
-            {
-                test: /\.js$/,
-                exclude: '/node_modules/',
-                use: 'babel-loader'
-            },
-            {
-                test: /\.(png|svg|jpe?g|gif)$/i,
-                use: [
-                    'file-loader?name=images/[name].[ext]',
-                    //'file-loader?name=[name].[ext]&outputPath=images/&publicPath=images/',
-                    'image-webpack-loader'
-                    ]
-            }
+  entry: {
+    app: './src/app.js',
+    contact: './src/contact.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }, {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        use: 'babel-loader'
+      }, {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        use: [
+          'file-loader?name=images/[name].[ext]',
+          //'file-loader?name=[name].[ext]&outputPath=images/&publicPath=images/',
+          'image-webpack-loader'
         ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-        // port: 9000,
-        // stats: "errors-only",
-        hot: true,
-        open: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Project Demo',
-            // minify: {
-            //     collapseWhitespace: true
-            // },
-            hash: true,
-            excludeChunks: ['contact'],
-            template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Contact Page',
-            hash: true,
-            chunks: ['contact'],
-            filename: 'contact.html',
-            template: './src/contact.html'
-        }),
-        new ExtractTextPlugin({
-            filename: 'app.css',
-            disable: true,
-            allChunks: true,
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
+      }
     ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    // port: 9000, stats: "errors-only",
+    hot: true,
+    open: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Project Demo',
+      // minify: {     collapseWhitespace: true },
+      hash: true,
+      excludeChunks: ['contact'],
+      template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
+    }),
+    new HtmlWebpackPlugin({title: 'Contact Page', hash: true, chunks: ['contact'], filename: 'contact.html', template: './src/contact.html'}),
+    new ExtractTextPlugin({filename: 'app.css', disable: true, allChunks: true}),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
 }
